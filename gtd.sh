ac5a01b665377245d60a038ce47c03296de179a1
todd <todd.kennedy@gmail.com>
2011-03-22 13:00:25 -0400

    adding test data generator

---
+++ gtd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p /tmp/testdata/Movies
+mkdir /tmp/testdata/TV
+touch /tmp/testdata/Movies/Aliens.mp4
+touch /tmp/testdata/Movies/Empire\ Strikes\ Back.mp4
+touch /tmp/testdata/TV/Community\ S01E22.mp4
+touch /tmp/testdata/TV/The\ Simpsons\ S22E05.mp4
